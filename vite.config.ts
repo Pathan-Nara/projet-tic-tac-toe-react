4a991022d2f4d90d21173d47448fef2556e297f3
Para <nathan.para@coda-student.school>
2025-12-26 23:31:16 +0100

    fix truc bete je suis très bete...

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-base: '/projet-tic-tac-toe-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/projet-tic-tac-toe-react'
 })
